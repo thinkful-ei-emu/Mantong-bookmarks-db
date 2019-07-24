cfe698ed59e0043789d651b56e1e45d66a621384
Mantong Zhao <zhaomantong0203@gmail.com>
2019-07-24 11:44:55 -0700

    seed

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,6 @@
+INSERT INTO bookmarks(id,title,url,description,rating)
+values
+(1,'First bookmark!','www.hello.com','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',5),
+(2,'Second bookmark!','www.News.com','Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.',4),
+(3,'Third bookmark!','www.Listicle.com','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.',3),
+(4,'Fourth bookmark!','www.Story.com','Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?',2),
--- src/app.js
+++ src/app.js
@@ -17,17 +17,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: 'Unauthorized request' });
-  }
-  // move to the next middleware
-  next();
-});
 app.use(bookmarkRouter);
 
 app.get('/bookmarks', (req, res, next) => {
